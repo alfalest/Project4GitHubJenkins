13eff78ad97257e2cf7d59ec226cd55049c66c42
alfalest <alfaini01@gmail.com>
2020-01-15 22:08:23 +0700

    Saya gabungin program luas dan kelilingnya dalam satu fle java

--- Triangle.java
+++ Triangle.java
@@ -1,21 +1,45 @@
 package Triangle;
 
+import static Triangle.LuasKeliling.a;
 import java.util.Scanner;
 public class Triangle {
+    static int aL, tL, aK, tK, sK, L, K, luas, keliling;
+
     public static void main (String[] args){
-        double a,t,L;
-        System.out.println("===Luas Segitiga===");
-        Scanner Masukkan= new Scanner(System.in);
+        //LUAS SEGITIGA
+        System.out.println("===LUAS SEGITIGA===");
+        Scanner Luas= new Scanner(System.in);
       
          System.out.print("Masukkan nilai a = ");
-         a=Masukkan.nextDouble();
+         aL=Luas.nextInt();
          System.out.print("Masukan nilai t = ");
-         t= Masukkan.nextDouble();
+         tL= Luas.nextInt();
         
-         L=a*t/2;
+         L=aL*tL/2;
          System.out.println("Luasnya ="+L);
         
          if(L>75)
+            System.out.println("Segitiga yg Kamu hitung adalah segitiga yg besar!!!");
+            else
+            System.out.println("Segitiga yg Kamu hitung adalah Segitiga yg kecil!!!");
+         
+         
+         //KELILING SEGITIGA
+         System.out.println("===KELILING SEGITIGA===");
+         Scanner Keliling = new Scanner(System.in);
+      
+         System.out.print("Masukkan nilai s = ");
+         sK = Keliling.nextInt();
+         System.out.print("Masukan nilai t = ");
+         tK = Keliling.nextInt();
+         System.out.print("Masukan nilai a = ");
+         aK = Keliling.nextInt();
+        
+         K = sK+tK+aK;
+         System.out.println("Kelilingnya ="+K);
+         
+   
+        if(K>75)
             System.out.println("Segitiga yg Kamu hitung adalah segitiga yg besar!!!");
             else
             System.out.println("Segitiga yg Kamu hitung adalah Segitiga yg kecil!!!");
